0cc4ad6dc7dca306ef70e9e062c26934d9ad80d9
Gabo Esquivel <contact@gaboesquivel.com>
2022-08-19 00:08:28 -0600

    chore: hide description

--- src/components/Services/ApiExplorer/ApiRequest/ApiRequestDetails.js
+++ src/components/Services/ApiExplorer/ApiRequest/ApiRequestDetails.js
@@ -6,7 +6,7 @@ class ApiRequestDetails extends Component {
     const styles = require('../ApiExplorer.scss');
     return (
       <div className={styles.apiRequestWrapper + ' ' + styles.apiContentPadd} style={{padding:0, border:0}}>
-        <div className={styles.apiRequestContent}>{this.props.description}</div>
+        {/* <div className={styles.apiRequestContent}>{this.props.description}</div> */}
         <div className={styles.apiHasura}>
           <a href="https://github.com/blockmatic/powerstack-explorer" target={'_blank'}>
             <i className="fa fa-github" />
